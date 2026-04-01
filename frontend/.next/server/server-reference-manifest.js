6d691b6cf56aea74cf042cad81aa3a98c0f85a84
Galust Minasov <galustminasov@MacBook-Pro-Galust.local>
2026-04-01 21:59:18 +0300

    Build frontend

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"TkKgU2yQVwTAC8RI7BHC7kaPxf1BYUOUM/WIR1Di2Yo=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"X7gOisGdSpmOLJEfpEE6m4HHiDg5N1YaUrJLJ3wYpi8=\"}"
