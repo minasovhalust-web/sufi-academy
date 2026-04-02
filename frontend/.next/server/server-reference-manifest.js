56d97c14569fea7c0fb0db639c0e7f31086b80ee
Galust Minasov <galustminasov@MacBook-Pro-Galust.local>
2026-04-02 20:02:24 +0300

    Build for muzasufy.com

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"S6xBKuQ3dvKszsxWpOJvG++l/JDrXUNNA9oqNFxY8Ag=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"mUcBikvAdwLWnIB6sGSwJO1aEsfHPkiO9Td10WPGs28=\"}"
